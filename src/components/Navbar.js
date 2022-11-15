45560513d1d8be057d7ffc196b405b4c50e5bcf1
Oluwabori Ige <igeoluwabori@gmail.com>
2022-11-16 00:17:45 +0100

    NewUpdate

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,14 @@ import React from "react";
 export default function NavBar() {
   return (
     <header className="bg-gray-100 h-30 p-3 lg:mr-10 ">
+      <h2 className="font-family Poppins">Devchanllenges.io</h2>
       <nav className=" font-semibold text-gray-400 ">
         <ul className="flex justify-center text-sm items-center space-x-2 lg:flex lg:flex-col lg:space-y-7 lg:px-8 lg:pt-14 lg:text-lg">
           <br />
           <li>Colors</li>
-          <li>Typography</li>
+          {/* <li>Typography</li>
           <li>Spaces</li>
-          <li>Buttons</li>        
+          <li>Buttons</li>         */}
           <li>Inputs</li>
           <li>Grid</li>
         </ul>
